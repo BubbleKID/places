580568c2eceb1d9d4df4a2f7be625120d7bc1663
Mark <fqchenxin@gmail.com>
2019-06-06 15:03:20 +1000

    added .env

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -16,7 +16,7 @@ class MainContent extends Component {
                 </Breadcrumb>
                 <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
                     <h1>London</h1>
-                    <img className="cityImage" src={london} style={{ marginBottom: 10}}></img>
+                    <img className="cityImage" alt="london" src={london} style={{ marginBottom: 10}}></img>
                     <p><strong>London </strong>is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
                     <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
                     <ButtonGroup className="buttonGroup">
